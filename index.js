823dd1f348dd3b85bfa32d4783e6faad008b7ea2
Luckner Jr <lucknerjb@gmail.com>
2014-09-30 20:00:04 -0400

    Got it working

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var config = require('./config/config.json');
 var _ = require('underscore');
 Tail = require('tail').Tail;
 
+// Vars
+var tails = {};
 var _sockets = {};
 
 // Views
@@ -22,11 +24,13 @@ app.get('/', function(req, res){
 app.get('/api/streams', function(req, res) {
    var streams = config.streams;
 
+   // @TODO
    // Delete keys that are internal
-   _.each(streams, function(stream) {
-      delete stream.path;
-      delete stream.custom;
-   });
+   // var streams = _.clone(config.streams);
+   // _.each(streams, function(stream) {
+   //    delete stream.path;
+   //    delete stream.custom;
+   // });
 
    res.json(streams);
 });
@@ -36,6 +40,8 @@ app.get('*', function(req, res) {
 });
 
 io.on('connection', function(socket){
+   // buildSocketEvents(socket);
+
    socket.on('identify', function(data) {
       // Store the socket
       _sockets[data.identifier] = socket;
@@ -44,28 +50,55 @@ io.on('connection', function(socket){
       socket.emit('identified', {status: 'success', identifier: data.identifier});
    });
 
+   socket.on('stream.listen', function(data) {
+      tailStream(data.identifier, data.stream_id);
+   });
+
    socket.on('disconnect', function(data) {
       if (typeof _sockets[data.identifier] !== 'undefined') {
          _sockets[data.identifier].disconnect();
       }
    });
 
-   socket.on('connect_stream', function(sockdata) {
-      console.log(sockdata);
-      tail = new Tail("/var/log/nginx/error.log");
-      var identifier = sockdata.identifier;
+  //  socket.on('connect_stream', function(sockdata) {
+  //     var identifier = sockdata.identifier;
+  //     var stream = sockdata.stream;
 
-      tail.on("line", function(data) {
-        console.log(data);
-        _sockets[identifier].emit('stream_data', {line: data, identifier: identifier, stream: sockdata.stream});
-      });
+  //     var file = getStreamPath(sockdata.stream);
+  //     tails[stream] = new Tail(file);
 
-      tail.on("error", function(error) {
-        console.log('ERROR: ', error);
-      });
-  });
+  //     tails[stream].on("line", function(data) {
+  //        console.log('got line');
+  //       _sockets[stream].emit('stream_data', {line: data, identifier: identifier, stream: stream});
+  //     });
+
+  //     tails[stream].on("error", function(error) {
+  //       console.log('ERROR: ', error);
+  //     });
+  // });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+function tailStream(identifier, stream_id) {
+   var streams = config.streams;
+   var file_path = getStreamPath(stream_id);
+
+   // Create a new Tail object and listen for file changes
+   tails[stream_id] = new Tail(file_path);
+   tails[stream_id].on("line", function(data) {
+      console.log('got line');
+     io.to(_sockets[identifier].id).emit('stream.data', {line: data, stream_id: stream_id});
+   });
+
+   tails[stream_id].on("error", function(error) {
+     console.log('ERROR: ', error);
+   });
+}
+
+function getStreamPath(stream_id) {
+   var streams = config.streams;
+   return streams[stream_id].path;
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,10 @@
 var socket;
 var identifier;
+var _sockets = {};
 // var socketServer = '127.0.0';
+var streams = {};
+var current_stream_id;
+var stream_indices = {};
 
 function generateIdentifier() {
    var text = "";
@@ -22,23 +26,67 @@ function pad(n) {
     return ("0" + n).slice(-2);
 }
 
+function initSocket() {
+   // Create an identifier
+   identifier = generateIdentifier();
+
+   // Create a socket server
+   startSocket();
+
+   // Identify socket
+   socket.emit('identify', {identifier: identifier});
+   socket.on('identified', function(data) {
+      $('#socketStatus').html('Connected as [' + data.identifier + ']');
+   });
+
+   // Listen to data streams
+   socket.on('stream.data', function(data) {
+      console.log(data);
+      var host = 'localhost';
+      var now = new Date();
+      var hh = pad(now.getHours());
+      var mm = pad(now.getMinutes());
+      var ss = pad(now.getSeconds());
+
+      var timeMark = '[' + hh + ':' + mm + ':' + ss + '] ';
+
+      var line = "<table class='message'><tr><td width='1%' class='date'>" + timeMark + "</td><td width='1%' valign='top' class='host'><a href=?ip=" + host + ">" + host + "</a></td>";
+      line += "<td class='msg-text' width='98%'>" + data.line + "</td></tr>";
+      $('#stream-' + data.stream_id + ' .log').append(line);
+      // updateCount();
+
+      // Auto scroll to bottom
+      window.scrollTo(0,document.body.scrollHeight);
+   });
+}
+
 function disconnectSocket() {
    socket.emit('disconnect', {identifier: identifier});
    $('#socketStatus').html('Disconnected');
    $('#toolbar').css('background', '#933');
 }
 
-function identify() {
-   identifier = generateIdentifier();
-   socket.emit('identify', {identifier: identifier});
-   socket.on('identified', function(data) {
+/*
+function identify(identifier, stream_id) {
+   _sockets[stream_id].emit('identify', {identifier: identifier, stream: stream_id});
+   _sockets[stream_id].on('identified', function(data) {
       $('#socketStatus').html('Connected as [' + data.identifier + ']');
    });
 }
 
 function connectToStream(stream_id) {
-   socket.emit('connect_stream', {stream: stream_id, user: 'jeanbal', identifier: identifier});
-   socket.on('stream_data', function(data) {
+   // Get an identifier
+   var identifier = generateIdentifier();
+
+   // Get a socket and store it
+   var socket = startSocket();
+   _sockets[stream_id] = socket;
+
+   // Identify
+   identify(identifier, stream_id);
+
+   socket.emit('connect_stream.' + stream_id, {stream: stream_id, identifier: identifier});
+   socket.on('stream_data' + stream_id, function(data) {
       console.log(data);
       var host = 'localhost';
       var now = new Date();
@@ -55,6 +103,7 @@ function connectToStream(stream_id) {
       // updateCount();
    });
 }
+*/
 
 function displayStreams(streams) {
    _.each(streams, function(stream, index) {
@@ -64,30 +113,46 @@ function displayStreams(streams) {
    });
 }
 
-function toggleStream(stream) {
+function toggleStream(stream_id) {
    // Hide all streams and display this one
    $('.stream').hide();
 
    // Do we have this view?
-   if ($('#stream-' + stream).length == 0) {
-      createStreamView(stream);
+   if ($('#stream-' + stream_id).length == 0) {
+      createStreamView(stream_id);
       // fetchInitialData(stream.id);
    }
 
-   $('#stream-' + stream).show();
+   $('#stream-' + stream_id).show();
+
+   current_stream_id = stream_id;
 }
 
 function toggleStreamsContainer() {
    $('#connected-streams').slideToggle('slow');
 }
 
-function createStreamView(stream) {
+function createStreamView(stream_id) {
    var html = ' \
-   <div id="stream-' + stream + '" class="stream"> \
+   <div id="stream-' + stream_id + '" class="stream"> \
       <div class="table-responsive log"></div> \
    </div>';
 
    $('#streams').append(html);
+}
+
+function listenToStream(stream_id) {
+   socket.emit('stream.listen', {identifier: identifier, stream_id: stream_id});
+
+   createStreamView(stream_id);
+}
+
+function calculateStreamIndices() {
+   var index = 0;
+   _.each(streams, function(stream, stream_id) {
+      stream_indices[stream_id] = index;
+      index++;
+   });
 }
 
 $(document).ready(function() {
@@ -96,7 +161,9 @@ $(document).ready(function() {
       url: '/api/streams',
       type: 'GET',
       success: function(data) {
+         all_streams = data;
          displayStreams(data);
+         initSocket();
       },
       error: function(data) {
          alert('Could not fetch streams');
@@ -108,41 +175,93 @@ $(document).ready(function() {
       $('#connected-streams').slideToggle('slow');
    });
 
-   $(document).on('click', '.connect', function(e) {
-      e.preventDefault();
-      startSocket();
-      identify();
-      $(this).removeClass('connect').addClass('disconnect').html('Disconnect');
-   });
+   // $(document).on('click', '.connect', function(e) {
+   //    e.preventDefault();
+   //    startSocket();
+   //    identify();
+   //    $(this).removeClass('connect').addClass('disconnect').html('Disconnect');
+   // });
 
-   $(document).on('click', '.disconnect', function(e) {
-      e.preventDefault();
-      disconnectSocket();
-      $(this).removeClass('disconnect').addClass('connect').html('Connect');
-   });
+   // $(document).on('click', '.disconnect', function(e) {
+   //    e.preventDefault();
+   //    disconnectSocket();
+   //    $(this).removeClass('disconnect').addClass('connect').html('Connect');
+   // });
 
    $(document).on('click', '.connect-stream', function(e) {
       e.preventDefault();
       var self = $(this);
       var stream_id = self.data('stream');
 
-      // Connected to a socket?
-      if (!socket) {
-         alert('Please connect first');
-         return;
-      }
-
-      // Connected to stream?
-      // @TODO: Move the classes to config
-      var connected = self.hasClass('btn-success');
-
-      if (!connected) {
-         connectToStream(stream_id);
+      // If we are not listening to the stream, let's do so
+      if ('undefined' == typeof streams[stream_id]) {
          self.removeClass('btn-primary').addClass('btn-success');
+
+         // Add to active streams
+         var stream = all_streams[stream_id];
+         streams[stream_id] = stream.name;
+         var html = '<li><a href="#" class="switch-stream" data-name="' + stream.name + '" data-stream="' + stream.id + '">' + stream.name + '</a></li>';
+         $('#active-streams').append(html);
+
+         // Listen
+         listenToStream(stream_id);
+
+         // Last picked stream becomes the active one
+         toggleStream(stream_id);
+         $('#current-stream').html(stream.name);
+      }
+      // Remove the stream from the list of streams we listen for
+      else {
+         var index = streams.indexOf(stream_id);
+         streams.splice(index, 1);
+         self.removeClass('btn-success').addClass('btn-primary');
+
+         $('#active-streams').find('a[data-stream="' + stream_id + '"]').remove();
+
+         // muteStream(stream_id);
       }
 
-      // Show the stream and hide the streams container
+      calculateStreamIndices();
+   });
+
+   $(document).on('click', '.switch-stream', function(e) {
+      e.preventDefault();
+      var stream_id = $(this).data('stream');
       toggleStream(stream_id);
-      toggleStreamsContainer();
+      $('#current-stream').html($(this).html());
+   });
+
+   // Key binding for Left and Right arrow keys
+   $(document).keydown(function(e) {
+      // Left Arrow
+      if(e.ctrlKey && e.which == 37) {
+         var max_index = Object.keys(streams).length - 1;
+         var is_last = (current_stream_id === Object.keys(streams)[max_index]);
+
+         // If this is the first stream in the list, show the last one
+         var next = current_stream_id;
+         if (stream_indices[current_stream_id] === 0) {
+            next = Object.keys(streams)[max_index];
+         } else {
+            next = Object.keys(streams)[stream_indices[current_stream_id] - 1];
+         }
+      }
+      // Right Arrow
+      else if(e.ctrlKey && e.which == 39) {
+         var max_index = Object.keys(streams).length - 1;
+         var is_last = (current_stream_id === Object.keys(streams)[max_index]);
+
+         // If this is the last stream in the list, show the first one
+         var next = current_stream_id;
+         if (stream_indices[current_stream_id] === max_index) {
+            next = Object.keys(streams)[0];
+         } else {
+            next = Object.keys(streams)[stream_indices[current_stream_id] + 1];
+         }
+
+         // Show the view and change the toolbar
+         toggleStream(next);
+         $('#current-stream').html(streams[next]);
+      }
    });
 });
